c0f2c7c7dc240f8e2886a3ca01e671db0b6b4c10
douyunqian <douyunqian@zhizhangyi.com>
2018-04-20 14:46:09 +0800

    增加了快速排序

--- charpta/start01.py
+++ charpta/start01.py
@@ -13,3 +13,34 @@ b="12345"
 trans=maketrans(a,b)
 str = "this is string example....wow!!!"
 print str.translate(trans)
+a=["a","Python","456"]
+a=set(a)
+b=set(["a","Python","c"])
+print a.symmetric_difference(b)
+a={"a":"123"}
+a.setdefault("b","456")
+print(a)
+# Pyhon实现的快速排序算法
+
+a=[4,0,3,-143,5,31,8]
+print u"排序前:"
+print a
+
+def mkkl(a):
+    length=len(a)
+    for i in range(0,length-1):
+        for j in range(1,length-1):
+            if a[i]>a[j]:
+                a[j],a[i]=a[i],a[j]
+
+
+
+
+
+
+
+print u"排序后"
+mkkl(a)
+print a
+
+
